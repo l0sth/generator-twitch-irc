380b8029c5339e0ab5534923fac00bd3e5521e2f
Schmoopiie <Schmoopiie@gmail.com>
2014-09-07 05:00:46 -0400

    Added channel name as a param for 'hosted'.

--- event/templates/_hosted.coffee
+++ event/templates/_hosted.coffee
@@ -3,9 +3,10 @@ module.exports = (client) ->
   ###*
   Room is now hosted by someone else.
   @event hosted
+  @params {string} channel
   @params {string} username
   @params {string} viewers count
   ###
-  client.addListener "hosted", (username, viewers) ->
+  client.addListener "hosted", (channel, username, viewers) ->
     # Do your stuff.
   return
--- event/templates/_hosted.js
+++ event/templates/_hosted.js
@@ -2,10 +2,11 @@ module.exports = function(client) {
     /**
      * Room is now hosted by someone else.
      * @event hosted
+     * @params {string} channel
      * @params {string} username
      * @params {string} viewers count
      */
-    client.addListener('hosted', function (username, viewers) {
+    client.addListener('hosted', function (channel, username, viewers) {
         // Do your stuff.
     });
 };
